c266bca5cf5243d05156021c5eacd8db8efff5fa
Saeid Kasiri <s.k.flower@gmail.com>
2025-06-13 02:25:40 +0330

    Remove create_post_types

--- includes/class-gicapi-activator.php
+++ includes/class-gicapi-activator.php
@@ -7,48 +7,8 @@ class GICAPI_Activator
 {
     public static function activate()
     {
-        self::create_post_types();
         self::create_options();
         flush_rewrite_rules();
-    }
-
-    private static function create_post_types()
-    {
-        // Register category post type
-        register_post_type('gic_cat', array(
-            'labels' => array(
-                'name' => __('Gift-i-Card Categories', 'gift-i-card'),
-                'singular_name' => __('Gift-i-Card Category', 'gift-i-card')
-            ),
-            'public' => false,
-            'show_ui' => false,
-            'show_in_menu' => false,
-            'supports' => array('title')
-        ));
-
-        // Register product post type
-        register_post_type('gic_prod', array(
-            'labels' => array(
-                'name' => __('Gift-i-Card Products', 'gift-i-card'),
-                'singular_name' => __('Gift-i-Card Product', 'gift-i-card')
-            ),
-            'public' => false,
-            'show_ui' => false,
-            'show_in_menu' => false,
-            'supports' => array('title')
-        ));
-
-        // Register variant post type
-        register_post_type('gic_var', array(
-            'labels' => array(
-                'name' => __('Gift-i-Card Variants', 'gift-i-card'),
-                'singular_name' => __('Gift-i-Card Variant', 'gift-i-card')
-            ),
-            'public' => false,
-            'show_ui' => false,
-            'show_in_menu' => false,
-            'supports' => array('title')
-        ));
     }
 
     private static function create_options()
